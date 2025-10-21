a5f139291a0bea976777385bc2a0eb7a035acc11
KaasLarsen <vallelarsen@yahoo.dk>
2025-10-21 14:13:28 +0200

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,13 @@
-import createMDX from '@next/mdx';
-import remarkGfm from 'remark-gfm';
+import createMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-  }
-});
+  options: { remarkPlugins: [remarkGfm] }
+})
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['ts', 'tsx', 'mdx'],
-  experimental: {
-    mdxRs: true
-  }
-};
+  pageExtensions: ['ts', 'tsx', 'mdx']
+}
 
-export default withMDX(nextConfig);
+export default withMDX(nextConfig)
